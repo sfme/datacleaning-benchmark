4b57bfdb67f23a3f822f11944871d4df45a4a7b1
Simao Eduardo <s.eduardo@ed.ac.uk>
2017-06-15 13:46:06 +0100

    Example script: subtle changes to Adult Dataset UCI ML

--- test/adult_example.py
+++ test/adult_example.py
@@ -25,7 +25,7 @@ from collections import namedtuple
 name_file = "adult"
 
 # Data Path to Adult Datasets
-DATADIR_PATH = "your dataset directory here containing the adult.csv file"
+DATADIR_PATH = "your data directory here, where we can find the adult dataset from UCI ML Repo"
 
 # Load Clean Version of Adult Dataset from UCI ML Repository (.CSV file)
 df_data = pd.read_csv(DATADIR_PATH + "/adult.csv")
@@ -95,16 +95,16 @@ cat_array_bool = (df_data.dtypes.apply(lambda t: str(t)) == 'category').values
 
 # Define Categorical Noise Model
 cat_mdl_overt = CategoricalNoiseModel((df_data.shape[0], len(cat_feat_names)), cats_names, 
-                                cats_probs_list=[], typo_prob=0.15) 
+                                cats_probs_list=[], typo_prob=0.70) 
 
 # Define Numerical Noise Model
 num_mdl_overt = ZipfNoiseModel((df_data.shape[0], len(num_feat_names)), z=1.8, scale=stds_df,
                          int_cast=np.ones(len(num_feat_names), dtype=bool), active_neg=True)
 
-# Define Mixed Model (0.10*0.30 = 0.03 of cells noised, and 0.10 of tuples)
+# Define Mixed Model (0.10*0.10 = 0.01 of cells noised, and 0.10 of tuples)
 mix_mdl_overt = MixedNoiseTupleWiseModel(df_data.shape, cat_array_bool, 
                                          idx_map_cat, idx_map_num, cat_mdl_overt,
-                                         num_mdl_overt, probability=0.10, p_row=0.30)
+                                         num_mdl_overt, probability=0.10, p_row=0.10)
 
 # Apply Noising of Data
 noised_overt_outliers, _ = mix_mdl_overt.apply(df_data.values)
@@ -122,9 +122,7 @@ folder_path = DATADIR_PATH + "/overt_noise/"
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
     
-noised_overt_outliers_df.to_csv(folder_path + name_file + "_noised_overt.csv", index=False)
-
-
+noised_overt_outliers_df.to_csv(folder_path + name_file + "_noised_overt.csv")
 
 df_changes_overt.to_csv(folder_path + name_file + "_df_changes_noise_overt_gt_errors.csv")
 
@@ -135,16 +133,16 @@ df_changes_overt.to_csv(folder_path + name_file + "_df_changes_noise_overt_gt_er
 
 # Define Categorical Noise Model
 cat_mdl_covert = CategoricalNoiseModel((df_data.shape[0], len(cat_feat_names)), cats_names, 
-                                       cats_probs_list=cats_probs, typo_prob=0.05) 
+                                       cats_probs_list=cats_probs, typo_prob=0.30) 
 
 # Define Numerical Noise Model
 num_mdl_covert = ZipfNoiseModel((df_data.shape[0], len(num_feat_names)), z=3, scale=stds_df/2,
                                 int_cast=np.ones(len(num_feat_names), dtype=bool), active_neg=True)
 
-# Define Mixed Model (0.10*0.30 = 0.03 of cells noised, and 0.10 of tuples)
+# Define Mixed Model (0.10*0.10 = 0.01 of cells noised, and 0.10 of tuples)
 mix_mdl_covert = MixedNoiseTupleWiseModel(df_data.shape, cat_array_bool, 
                                           idx_map_cat, idx_map_num, cat_mdl_covert,
-                                          num_mdl_covert, probability=0.10, p_row=0.30)
+                                          num_mdl_covert, probability=0.10, p_row=0.10)
 
 # Apply Noising of Data
 noised_covert_outliers, _ = mix_mdl_covert.apply(df_data.values)
@@ -162,7 +160,7 @@ folder_path = DATADIR_PATH + "/covert_noise/"
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
     
-noised_covert_outliers_df.to_csv(folder_path + name_file + "_noised_covert.csv", index=False)
+noised_covert_outliers_df.to_csv(folder_path + name_file + "_noised_covert.csv")
 
 df_changes_covert.to_csv(folder_path + name_file + "_df_changes_noise_covert_gt_errors.csv")
 
